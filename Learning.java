b86e9a158239d0a688a31b41df0e249ce4d7fd0f
RaM101507 <rj.mitchem92@gmail.com>
2023-04-17 20:16:56 -0400

    test 2

--- Learning.java
+++ Learning.java
@@ -7,40 +7,51 @@ import java.util.Scanner;
 public class Learning {
     
     public static void main(String[] args) {
-
+        
         Scanner myNum = new Scanner(System.in);
         Scanner myString = new Scanner(System.in);
         int answer = 0;
-    
-        System.out.println("Calculator");
+        boolean Quit = false;
+        while (!Quit){
             
-        System.out.println("Enter first number");
-    
-        int a = myNum.nextInt();
-    
-        System.out.println("Enter Second number");
-    
-        int b = myNum.nextInt();
-    
-        System.out.println("Enter operator");
-    
-        String op = myString.nextLine();
-    
-        switch(op){
-            case "+":
+            System.out.println("Calculator");
+            System.out.println("Enter first number");
+            
+            int a = myNum.nextInt();
+            
+            System.out.println("Enter Second number");
+            
+            int b = myNum.nextInt();
+            
+            
+            System.out.println("Enter operator");
+            
+            String op = myString.nextLine();
+            
+            switch(op){
+                case "+":
                 answer = a+b;
                 break;
-            case "-":
+                case "-":
                 answer = a-b;
                 break;
-            case "/":
+                case "/":
                 answer = a/b;
                 break;
-            case "*":
+                case "*":
                 answer = a*b;
                 break;
-          }
-            //test
-        System.out.println(answer);
+            }
+            //output
+            System.out.println(answer);
+            System.out.println("Quit now? y or n");
+            String QuitA = myString.nextLine();
+            
+            if(QuitA == "y"){
+                Quit = true;
+            }
+            
+            
+        }
     }    
 }

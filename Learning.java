ab2a2ca0bc1b72ebf6bf98e710d63dac07d14b8a
RaM101507 <rj.mitchem92@gmail.com>
2023-04-17 20:09:49 -0400

    test

--- Learning.java
+++ Learning.java
@@ -1,8 +1,46 @@
 package Learning;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 
 public class Learning {
-    public static void main(String[] args) {
-        
-    }
     
+    public static void main(String[] args) {
+
+        Scanner myNum = new Scanner(System.in);
+        Scanner myString = new Scanner(System.in);
+        int answer = 0;
+    
+        System.out.println("Calculator");
+            
+        System.out.println("Enter first number");
+    
+        int a = myNum.nextInt();
+    
+        System.out.println("Enter Second number");
+    
+        int b = myNum.nextInt();
+    
+        System.out.println("Enter operator");
+    
+        String op = myString.nextLine();
+    
+        switch(op){
+            case "+":
+                answer = a+b;
+                break;
+            case "-":
+                answer = a-b;
+                break;
+            case "/":
+                answer = a/b;
+                break;
+            case "*":
+                answer = a*b;
+                break;
+          }
+            //test
+        System.out.println(answer);
+    }    
 }
